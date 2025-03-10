1abdbb30a7a85cec8c80bc326f8af4af4c60c6a3
Amal Thomas <thomasamal61@gmail.com>
2025-03-10 09:31:35 +0530

    update slkill

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -22,6 +22,7 @@ function Skills() {
       </div>
       <hr />
       <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="MERN" />
         <SkillList src={checkMarkIcon} skill="React" />
         <SkillList src={checkMarkIcon} skill="Angular" />
         <SkillList src={checkMarkIcon} skill="Java" />
