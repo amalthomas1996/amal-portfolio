ec2267c960525b602b3d81e750f9b561deebffd6
Amal Thomas <thomasamal61@gmail.com>
2025-03-10 09:25:05 +0530

    add projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -5,12 +5,28 @@ import advice from "../../assets/advice.png";
 import bmw from "../../assets/bmw.png";
 import gemini from "../../assets/gemini1.png";
 import ProjectCard from "../../common/ProjectCard";
+import openarxe from "../../assets/openarxe.png";
+import dns from "../../assets/dns.png";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={dns}
+          link1="https://github.com/amalthomas1996/menu-app?tab=readme-ov-file"
+          link2="https://menu-app-frontend-zcbz.onrender.com/"
+          h3="Menu App"
+          p="Menu App for Resto Cafe(MERN Stack)"
+        />
+        <ProjectCard
+          src={openarxe}
+          link1="https://openarxe.com/"
+          link2="https://openarxe.com/"
+          h3="OpenArxe"
+          p="Website for architecture firm(Freelance)"
+        />
         <ProjectCard
           src={youtube}
           link1="https://github.com/amalthomas1996/YoutubeClone"
